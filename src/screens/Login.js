4e40c764c6a3bec13a1336f18357eb499b518080
nathaniaelirica <nathaniaelirica@gmail.com>
2023-12-10 18:17:16 +0700

    feat: register login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -11,7 +11,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import { signIn } from "../../functions/functions.js";
 
-export default function App() {
+export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
