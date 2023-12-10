88f6ee1690258e7c5262be1d43eb77aa5f13b292
Annisa Rahmapuri <annisa.rahmapuri@gmail.com>
2023-12-10 17:43:00 +0700

    add fe-search

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, TextInput, ActivityIndicator } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, ActivityIndicator, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Ionicons } from '@expo/vector-icons';
 import Background from "../components/Background";
 import { handleSearch } from "../../functions/functions.js";
+
+import SearchBox from "../components/Box/SearchBox";
 
 export default function Search({ navigation }) {
   const [query, setSearchQuery] = useState("");
@@ -27,37 +29,47 @@ export default function Search({ navigation }) {
 
   return (
     <Background>
-      <View className="flex flex-row m-1 w-full">
-        <TouchableOpacity className="content-start mt-1" onPress={() => navigation.navigate("main")}>
-          <Ionicons name="arrow-back" size={25} color="black" />
+      <ScrollView>
+        <View className="flex flex-row m-1 w-full">
+          <TouchableOpacity className="content-start mt-1" onPress={() => navigation.navigate("main")}>
+            <Ionicons name="arrow-back" size={25} color="black" />
+          </TouchableOpacity>
+          <Text className="text-black text-center font-inter text-lg font-semibold mx-4">
+            Search
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          className="flex-row items-center justify-center p-3 rounded-lg m-6 bg-white border border-[#8b91a3]">
+          <Ionicons name="search-outline" size={22} color="black" />
+          <TextInput
+            className="flex-1 ml-8 text-[#8b91a3] text-base font-light"
+            placeholder="Cari restoran atau menu makanan"
+            placeholderTextColor="#8b91a3"
+            value={query}
+            onChangeText={(text) => setSearchQuery(text)}
+            editable={true}
+            onSubmitEditing={onSearch}
+          />
         </TouchableOpacity>
-        <Text className="text-black text-center font-inter text-lg font-semibold mx-4">
-          Search
-        </Text>
-      </View>
 
-      <TouchableOpacity
-        className="flex-row items-center justify-center p-3 rounded-lg m-6 bg-white border border-[#8b91a3]"
-      >
-        <Ionicons name="search-outline" size={22} color="black" />
-        <TextInput
-          className="flex-1 ml-8 text-[#8b91a3] text-base font-light"
-          placeholder="Cari restoran atau menu makanan"
-          placeholderTextColor="#8b91a3"
-          value={query}
-          onChangeText={(text) => setSearchQuery(text)}
-          editable={true}
-          onSubmitEditing={onSearch}
-        />
-      </TouchableOpacity>
+        <View className="flex flex-col py-2 items-center justify-center">
+          {searchResults.map((result, id) => (
+            <SearchBox
+              imageSource={require('../../assets/fast_food.jpg')}
+              key={result.id}
+              distance={5} 
+              title={result.title}
+              price={88.5}
+              rating={result.rating}
+              totalReviews={2395}
+              backgroundColor="bg-yellow text-black"
+              onPress={() => navigation.navigate("restaurantpage")}
+            /> 
+          ))}
+          </View>
 
-      {/* Tampilkan hasil pencarian */}
-      <View>
-        {searchResults.map((result, index) => (
-          <Text key={index}>{result.title}</Text>
-        ))}
-      </View>
-    
+      </ScrollView>
       <StatusBar style="auto" />
     </Background>
   );
